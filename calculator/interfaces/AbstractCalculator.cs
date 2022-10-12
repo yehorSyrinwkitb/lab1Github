2d751774127e4566e5be03bbf40abaddbce1eb8e
yehor <yehor.syrin@gmail.com>
2022-10-12 22:23:02 +0300

    Initial commit

---
+++ calculator/interfaces/AbstractCalculator.cs
@@ -0,0 +1,17 @@
+﻿namespace calculator.interfaces;
+
+public abstract class AbstractCalculator
+{
+    //available operations
+    private Dictionary<string, IOperation> avaliableOperations = new Dictionary<string, IOperation>();
+
+    //method for adding new operations
+    public void AddOperation(IOperation operation)
+    {
+        avaliableOperations.Add(operation.GetKey(), operation);
+    }
+
+    //main method for performing each operation
+    public abstract double PerformCalculation(string operation, double[] values);
+    
+}
---
+++ calculator/interfaces/Operation.cs
@@ -0,0 +1,9 @@
+﻿namespace calculator.interfaces;
+
+public interface IOperation
+{
+    //method for calculation
+    public double Perform(double[] values);
+    //method for operation search
+    public string GetKey();
+}
